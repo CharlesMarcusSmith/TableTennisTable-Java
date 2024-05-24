9150954f9f072065f2f4fef738bb0792e8fa69db
Charles <charliemarcussmith@googlemail.com>
2024-05-24 12:34:02 +0100

    Testing 1 base goals reached. Moving on to stretch goals.

--- app/src/test/java/tabletennistable/AppTest.java
+++ app/src/test/java/tabletennistable/AppTest.java
@@ -7,6 +7,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import static org.mockito.ArgumentMatchers.contains;
+
 public class AppTest {
     @Mock
     LeagueRenderer renderer;
@@ -16,6 +18,14 @@ public class AppTest {
         MockitoAnnotations.initMocks(this);
     }
 
+    public void addTestPlayers(App app){
+        app.sendCommand("add player Jack");
+        app.sendCommand("add player Jill");
+        app.sendCommand("add player Josh");
+        app.sendCommand("add player Joey");
+        app.sendCommand("add player Jojo");
+        app.sendCommand("add player Jeff");
+    }
     @Test
     public void testPrintsCurrentState()
     {
@@ -27,4 +37,46 @@ public class AppTest {
 
         Assert.assertEquals("Rendered League", app.sendCommand("print"));
     }
+
+    @Test
+    public void testGetRows()
+    {
+        initMocks();
+        League league = new League();
+        App app = new App(league, renderer, null);
+        addTestPlayers(app);
+
+        Assert.assertEquals(3, league.getRows().stream().count());
+    }
+
+    @Test
+    public void testAddPlayer()
+    {
+        initMocks();
+        League league = new League();
+        App app = new App(league, renderer, null);
+
+        //Adding a player
+        app.sendCommand("add player Tom");
+
+        Assert.assertEquals("Tom", league.getRows().get(0).getPlayers().get(0));
+    }
+
+    @Test
+    public void testRecordWin()
+    {
+        initMocks();
+        League league = new League();
+        App app = new App(league, renderer, null);
+        addTestPlayers(app);
+
+        // Jack is original winner:
+        // Assert.assertEquals("Jack", league.getWinner()); = True
+
+        //Jill becomes winner:
+        app.sendCommand("record win Jill Jack");
+        Assert.assertEquals("Jill", league.getWinner());
+    }
+
+
 }
